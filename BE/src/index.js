62df7cd9eb3a113c838898856eef8c0d02b93b43
Youngchan Shin <chan8149@naver.com>
2022-08-17 20:28:19 +0900

    Install nodemon

--- BE/src/index.js
+++ BE/src/index.js
@@ -2,10 +2,6 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
-
 app.listen(4000, (err, res) => {
   console.log('Listening...');
 });
