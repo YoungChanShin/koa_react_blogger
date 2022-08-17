309c8b1a27beaf3106dbe8d960ec4a84c86d3b76
Youngchan Shin <chan8149@naver.com>
2022-08-17 21:36:34 +0900

    modulize route and Install koa-bodyparser

--- BE/src/api/index.js
+++ BE/src/api/index.js
@@ -1,8 +1,12 @@
 const Router = require('koa-router');
+
+const posts = require('./posts');
+
 const api = new Router();
 
 api.get('/conn', (ctx) => {
   ctx.body = 'conn 성공';
 });
 
+api.use('/posts', posts.routes());
 module.exports = api;
