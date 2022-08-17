d015f123e5137a1a2e80dca94347e3e17c5a65a8
Youngchan Shin <chan8149@naver.com>
2022-08-17 20:46:02 +0900

    Install koa-router

--- BE/src/index.js
+++ BE/src/index.js
@@ -1,7 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
+router.use('/api', api.routes());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, (err, res) => {
   console.log('Listening...');
 });
