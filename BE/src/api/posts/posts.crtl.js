7dcbab088e32c3b353a3150dde856bcae19efa09
Youngchan Shin <chan8149@naver.com>
2022-08-20 21:46:00 +0900

    Add read, write function

--- BE/src/api/posts/posts.crtl.js
+++ BE/src/api/posts/posts.crtl.js
@@ -1,10 +1,42 @@
 import Post from '../../models/post';
 
-export const write = (ctx) => {};
-export const list = (ctx) => {};
-export const read = (ctx) => {};
-export const remove = (ctx) => {};
-export const update = (ctx) => {};
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status(404);
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+export const remove = async (ctx) => {};
+export const update = async (ctx) => {};
 // exports.list = (ctx) => {};
 // exports.write = (ctx) => {};
 // exports.read = (ctx) => {};
