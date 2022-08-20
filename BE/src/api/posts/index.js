0c44ffd58e3701834cfb9307cfc2060fcead8174
Youngchan Shin <chan8149@naver.com>
2022-08-20 21:00:07 +0900

    make post model

--- BE/src/api/posts/index.js
+++ BE/src/api/posts/index.js
@@ -6,7 +6,6 @@ posts.get('/', postsCrtl.list);
 posts.post('/', postsCrtl.write);
 posts.get('/:id', postsCrtl.read);
 posts.delete('/:id', postsCrtl.remove);
-posts.put('/:id', postsCrtl.replace);
 posts.patch('/:id', postsCrtl.update);
 
 export default posts;
--- BE/src/api/posts/posts.crtl.js
+++ BE/src/api/posts/posts.crtl.js
@@ -1,8 +1,9 @@
-export const list = (ctx) => {};
+import Post from '../../models/post';
+
 export const write = (ctx) => {};
+export const list = (ctx) => {};
 export const read = (ctx) => {};
 export const remove = (ctx) => {};
-export const replace = (ctx) => {};
 export const update = (ctx) => {};
 // exports.list = (ctx) => {};
 // exports.write = (ctx) => {};
